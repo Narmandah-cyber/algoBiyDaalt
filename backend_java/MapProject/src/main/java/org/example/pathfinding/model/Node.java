43f76f276a788e79192c77f62f0e900acd558fb9
Assaaku <110076008+Assaaku@users.noreply.github.com>
2025-11-13 20:38:40 +0800

    added java conversion files

---
+++ backend_java/MapProject/src/main/java/org/example/pathfinding/model/Node.java
@@ -0,0 +1,43 @@
+package org.example.pathfinding.model;
+
+import java.util.Objects;
+
+public class Node {
+    private final double lon;
+    private final double lat;
+
+    public Node(double lon, double lat) {
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double[] toArray() {
+        return new double[]{lon, lat};
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Node node)) return false;
+        return Double.compare(node.lon, lon) == 0 &&
+                Double.compare(node.lat, lat) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lon, lat);
+    }
+
+    @Override
+    public String toString() {
+        return "Node(" + lon + ", " + lat + ")";
+    }
+}
